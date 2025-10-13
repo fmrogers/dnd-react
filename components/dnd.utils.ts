1435e521f746a67ab63d587d8c013ed0e2320131
Frederick M. Rogers <fmrogers@carbonium.no>
2025-10-13 11:17:54 +0200

    Removed unused code

--- components/dnd.utils.ts
+++ components/dnd.utils.ts
@@ -1,5 +1,3 @@
-import type { DragOverState } from '@/components/dnd.types';
-
 export const reorder = <T>(list: T[], from: number, to: number): T[] => {
   if (from === to) return list;
 
@@ -8,29 +6,6 @@ export const reorder = <T>(list: T[], from: number, to: number): T[] => {
   copy.splice(to, 0, moved);
 
   return copy;
-};
-
-export const computeTargetIndex = (originalIndex: number, over: DragOverState, length: number): number => {
-  const { index: hoverIndex, position } = over;
-
-  let target =
-    position === 'before'
-      ? hoverIndex > originalIndex
-        ? hoverIndex - 1
-        : hoverIndex
-      : hoverIndex > originalIndex
-      ? hoverIndex
-      : hoverIndex + 1;
-
-  if (target < 0) {
-    target = 0;
-  }
-
-  if (target > length - 1) {
-    target = length - 1;
-  }
-
-  return target;
 };
 
 export const computeTargetFromBoundary = (originalIndex: number, boundary: number): number => {
